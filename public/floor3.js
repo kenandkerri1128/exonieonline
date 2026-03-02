b85dff31db13a62d799653671e8ef1a6bd4b8cdb
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-02 20:07:43 +0800

    Create floor3.js

---
+++ public/floor3.js
@@ -0,0 +1,13 @@
+var floor3MapData = {
+    "id": "floor3",
+    "name": "Floor 3",
+    "image": "floor3.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['floor3MapData'] = floor3MapData;
