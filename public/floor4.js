82f0943afafc1ee29a3d5607ca74408208a7d23a
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-05 04:02:22 +0800

    Create floor4.js

---
+++ public/floor4.js
@@ -0,0 +1,13 @@
+var floor4MapData = {
+    "id": "floor4",
+    "name": "Floor 4",
+    "image": "floor4.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['floor4MapData'] = floor4MapData;
