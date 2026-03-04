3a3a13d04c24445a3155e5c6443199865726c7dd
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-05 04:25:39 +0800

    Create floor5.js

---
+++ public/floor5.js
@@ -0,0 +1,13 @@
+var floor5MapData = {
+    "id": "floor5",
+    "name": "Floor 5",
+    "image": "floor5.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['floor5MapData'] = floor5MapData;
