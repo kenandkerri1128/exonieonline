2113e8a49712fafc84e6632ebd68f3f491316d54
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-14 00:42:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3582,7 +3582,7 @@ socket.on('requestSell', async (data) => {
     });
 });
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Exonie server running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () => console.log(`Exonie server running on port ${PORT} (0.0.0.0)`));
 
 
 
