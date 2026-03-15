a1b49484f6ab6beb18963b456c3752abc2ce4f57
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-16 00:52:40 +0800

    Create floor6.js

---
+++ public/floor6.js
@@ -0,0 +1,13 @@
+var floor6MapData = {
+    "id": "floor6",
+    "name": "Floor 6",
+    "image": "floor6.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['floor6MapData'] = floor6MapData;
