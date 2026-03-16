7660608ba8f9d001de0b0944bed45e3affb0c566
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-17 02:41:06 +0800

    Create floor7.js

---
+++ public/floor7.js
@@ -0,0 +1,13 @@
+var floor7MapData = {
+    "id": "floor7",
+    "name": "Floor 7",
+    "image": "floor7.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['floor7MapData'] = floor7MapData;
