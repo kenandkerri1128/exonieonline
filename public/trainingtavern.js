8a3dba9c6447716d66334d0713228bbc3477c901
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-18 01:01:32 +0800

    Update trainingtavern.js

--- public/trainingtavern.js
+++ public/trainingtavern.js
@@ -4,7 +4,32 @@ var trainingtavernMapData = {
     "image": "trainingtavern.png",
     "spawnX": 960,
     "spawnY": 1000,
-    "collisions": [],
+    "collisions": [
+        {
+            "x": 44,
+            "y": 6,
+            "w": 1947,
+            "h": 240
+        },
+        {
+            "x": 7,
+            "y": 8,
+            "w": 40,
+            "h": 1325
+        },
+        {
+            "x": 56,
+            "y": 1242,
+            "w": 1933,
+            "h": 75
+        },
+        {
+            "x": 1938,
+            "y": 268,
+            "w": 60,
+            "h": 978
+        }
+    ],
     "teleports": [],
     "normalSpawns": [],
     "miniBossSpawns": [],
