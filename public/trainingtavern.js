cc97f8746991caad5c0f8e64dda35b3d4726b008
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-18 00:32:34 +0800

    Create trainingtavern.js

---
+++ public/trainingtavern.js
@@ -0,0 +1,13 @@
+var trainingtavernMapData = {
+    "id": "trainingtavern",
+    "name": "Training Tavern",
+    "image": "trainingtavern.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['trainingtavernMapData'] = trainingtavernMapData;
