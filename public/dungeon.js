d5cf2376b2046af6f201334adaac1c058334981d
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-19 18:17:15 +0800

    Create dungeon.js

---
+++ public/dungeon.js
@@ -0,0 +1,13 @@
+var dungeon1MapData = {
+    "id": "dungeon1",
+    "name": "Dungeon 1",
+    "image": "dungeon1.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['dungeon1MapData'] = dungeon1MapData;
