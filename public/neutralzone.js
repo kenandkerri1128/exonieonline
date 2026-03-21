b10b7385b83f4325c711755ed9419bb1f5409240
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-21 21:00:36 +0800

    Create neutralzone.js

---
+++ public/neutralzone.js
@@ -0,0 +1,13 @@
+var neutralzoneMapData = {
+    "id": "neutralzone",
+    "name": "Neutral Zone",
+    "image": "neutralzone.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['neutralzoneMapData'] = neutralzoneMapData;
