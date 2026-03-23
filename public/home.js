f7dd537ced36f52ca4b2030b7170c3d816139d46
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-24 04:11:25 +0800

    Create home.js

---
+++ public/home.js
@@ -0,0 +1,13 @@
+var homeMapData = {
+    "id": "home",
+    "name": "Home",
+    "image": "home.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['homeMapData'] = homeMapData;
