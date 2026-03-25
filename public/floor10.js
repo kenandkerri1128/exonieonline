772c712e9128c5cd4c54c067259c6f5a327cfeda
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-25 17:23:44 +0800

    Create floor10.js

---
+++ public/floor10.js
@@ -0,0 +1,13 @@
+var floor10MapData = {
+    "id": "floor10",
+    "name": "Floor 10",
+    "image": "floor10.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['floor10MapData'] = floor10MapData;
