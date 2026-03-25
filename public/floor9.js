ead675f77c50857193e496ceff8e72f71cb76321
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-25 17:23:14 +0800

    Create floor9.js

---
+++ public/floor9.js
@@ -0,0 +1,13 @@
+var floor9MapData = {
+    "id": "floor9",
+    "name": "Floor 9",
+    "image": "floor9.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['floor9MapData'] = floor9MapData;
