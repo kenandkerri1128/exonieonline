e04a4049cfd01a9fe8df2823f6a47a0449ca74fd
kenandkerri1128 <johnxenieayag@gmail.com>
2026-03-25 17:22:24 +0800

    Create floor8.js

---
+++ public/floor8.js
@@ -0,0 +1,13 @@
+var floor8MapData = {
+    "id": "floor8",
+    "name": "Floor 8",
+    "image": "floor8.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['floor8MapData'] = floor8MapData;
