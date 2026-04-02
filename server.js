5d1320e39db55515b26233e24a4a31ab44e1cabd
kenandkerri1128 <johnxenieayag@gmail.com>
2026-04-03 04:36:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1050,7 +1050,6 @@ function generateLoot(monster) {
     }
     
     return item;
-}
 function generateTavernLoot(level, rarity) {
     const types = ['necklace', 'ring', 'earrings'];
     const type = types[Math.floor(Math.random() * types.length)];
