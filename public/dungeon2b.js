989cbb02bbc6c806449eda5d5e6f418f6ec200b3
kenandkerri1128 <johnxenieayag@gmail.com>
2026-04-06 02:53:04 +0800

    Create dungeon2b.js

---
+++ public/dungeon2b.js
@@ -0,0 +1,38 @@
+var dungeon2bMapData = {
+    "id": "dungeon2b",
+    "name": "Ancient Cave B",
+    "image": "dungeon2b.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [
+        {
+            "x": 44,
+            "y": 6,
+            "w": 1947,
+            "h": 240
+        },
+        {
+            "x": 7,
+            "y": 8,
+            "w": 40,
+            "h": 1325
+        },
+        {
+            "x": 56,
+            "y": 1242,
+            "w": 1933,
+            "h": 75
+        },
+        {
+            "x": 1938,
+            "y": 268,
+            "w": 60,
+            "h": 978
+        }
+    ],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['dungeon2bMapData'] = dungeon2bMapData;
