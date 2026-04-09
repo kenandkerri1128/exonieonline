0b4989d9324750499b66f2422e9616e34b1b036b
kenandkerri1128 <johnxenieayag@gmail.com>
2026-04-10 02:29:11 +0800

    Create battlefield.js

---
+++ public/battlefield.js
@@ -0,0 +1,13 @@
+var battlefieldMapData = {
+    "id": "battlefield",
+    "name": "Battle Field",
+    "image": "battlefield.png",
+    "spawnX": 960,
+    "spawnY": 1000,
+    "collisions": [],
+    "teleports": [],
+    "normalSpawns": [],
+    "miniBossSpawns": [],
+    "floorBossSpawns": []
+};
+if(typeof window !== 'undefined') window['battlefieldMapData'] = battlefieldMapData;
